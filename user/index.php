d9f61aace7b7dcc80f5b512a3930933c4ae255c7
AfifuddinM <94724403+AfifuddinM@users.noreply.github.com>
2022-06-26 16:56:06 +0800

    Add files via upload

---
+++ user/index.php
@@ -0,0 +1,54 @@
+<?php 
+include ("../session.php");
+session_start();
+
+if (!isset($_SESSION['username'])) {
+       header('Location: ../login');
+		} 
+		
+?>
+<?php include('../include/navbarUser.php') ?>
+<!-- partial -->
+  <div class="main">
+<h1> E-Sukan Equipment Booking System</h1>
+<h3>Welcome <?php echo $_SESSION['username'];?></h3>
+<ul>
+    <li>user detail</li>
+    <li>view equipment</li>
+    <li>booking</li>
+  </ul>
+</div>
+</body>
+</html>
+
+
+
+
+   
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
