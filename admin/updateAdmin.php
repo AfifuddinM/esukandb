8680575b0f7a773cd11f2d08dd84b250198a223a
afifuddin24 <afifudidn15.am@gmail.com>
2022-07-12 13:32:25 +0800

    styling

--- admin/updateAdmin.php
+++ admin/updateAdmin.php
@@ -10,50 +10,44 @@ $res = mysqli_query($dbconn, $q) or die(mysqli_error($dbconn));
 $r = mysqli_fetch_assoc($res);
 //creating a detail table for a specific equipment
 ?>
-<style>
-    table {
-        border: 1px solid black;
-        margin-right: 20px;
-    }
-</style>
 <br>
 
 <div class="main" style="margin-left:5% ; margin-right:5%;">
     <h3 class="display-4">update Profile</h3>
     <form action="updateAdminProcess.php" method="POST">
-        <table width=98% style="margin-top:5% ;">
+        <table class="table " width=98% style="margin-top:5% ;">
             <tr>
                 <td>Admin id:</td>
-                <td><input type="text" name="userid" value="<?php echo $r['user_id']; ?>" readonly></td>
+                <td><input type="text" class="form-control" name="userid" value="<?php echo $r['user_id']; ?>" readonly></td>
             </tr>
             <tr>
                 <td>username:</td>
-                <td><input type="text" name="username" value="<?php echo $r['username']; ?>" readonly></td>
+                <td><input type="text" class="form-control" name="username" value="<?php echo $r['username']; ?>" readonly></td>
             </tr>
             <tr>
                 <td>Gender:</td>
-                <td><input type="text" name="Gender" value="<?php
-                                                            if ($r['gender'] == 1) {
-                                                                echo "Male";
-                                                            } else
-                                                                echo "Female";
-                                                            ?>" readonly></td>
+                <td><input type="text" class="form-control" name="Gender" value="<?php
+                                                                                    if ($r['gender'] == 1) {
+                                                                                        echo "Male";
+                                                                                    } else
+                                                                                        echo "Female";
+                                                                                    ?>" readonly></td>
             </tr>
             <tr>
                 <td>Phone Number: </td>
-                <td><input type="text" name="telephone" value="<?php echo $r['telephone']; ?>"></td>
+                <td><input type="text" class="form-control" name="telephone" value="<?php echo $r['telephone']; ?>"></td>
             </tr>
 
             <tr>
                 <td>Email Address:</td>
-                <td><input type="text" name="email" value="<?php echo $r['email']; ?>"></td>
+                <td><input type="text" class="form-control" name="email" value="<?php echo $r['email']; ?>"></td>
             </tr>
             <tr>
                 <td>Home Address:</td>
-                <td><input type="text" maxlength="255" name="address" value="<?php echo $r['address']; ?>" style="width:45% ; height:100px;"></td>
+                <td><input type="text" class="form-control" maxlength="255" name="address" value="<?php echo $r['address']; ?>" style="width:100% ; height:100px;"></td>
             </tr>
         </table>
-        <button type="submit" name="updatebtn">Update Button</button>
+        <button type="submit" class="btn btn-primary" name="updatebtn">Update Button</button>
     </form>
 </div>
 
